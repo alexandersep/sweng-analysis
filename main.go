c4357de5018638e39d3ba588b93570f674c16698
WesLemon <wesleyshaw25@gmail.com>
2022-11-15 16:25:31 +0000

    set up backend server with default metric data

--- main.go
+++ main.go
@@ -2,47 +2,60 @@ package main
 
 import (
 	"context"
-	"fmt"
+	"net/http"
 
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
 	"github.com/google/go-github/v48/github"
 )
 
-func main() {
+type metrics struct {
+	Owner          string         `json:"owner"`
+	Repo           string         `json:"repo"`
+	Languages      map[string]int `json:"languages"`
+	Commit_Average int            `json:"average_commits_this_year"`
+}
 
-	var owner = ""
-	print("Please enter a github username you would like to query, or type \"quit\" to quit: ")
-	fmt.Scan(&owner)
-	for owner != "quit" {
-		ctx := context.Background()
-		client := github.NewClient(nil)
-		//repos, _, err := client.Repositories.List(ctx, owner, nil)
-		/*if err != nil {
-			println("Error: ", err)
-			return
-		}
-		*/
-		input_repo := ""
-		print("Please enter a repo associated with that github username: ")
-		fmt.Scan(&input_repo)
+var user_metrics = metrics{}
 
-		commit_activity, _, err := client.Repositories.ListCommitActivity(ctx, owner, input_repo)
-		if err != nil {
-			println("Error: ", err.Error())
-			return
-		}
-		languages, _, err := client.Repositories.ListLanguages(ctx, owner, input_repo)
-		if err != nil {
-			println("Error: ", err.Error())
-			return
-		}
-		var commit_avg int = 0
-		for _, week := range commit_activity {
-			commit_avg += *week.Total
-		}
-		commit_avg /= 52
-		println("Languages: ", fmt.Sprint(languages), "\n",
-			"Average weekly commits over past year: ", commit_avg)
-		print("Please enter a github username you would like to query, or type \"quit\" to quit: ")
-		fmt.Scan(&owner)
+func getMetrics(context *gin.Context) {
+	context.Header("Access-Control-Allow-Origin", "http://localhost:8080")
+	context.IndentedJSON(http.StatusOK, user_metrics)
+}
+
+func populate_metrics(owner, repo string, languages map[string]int, commit_avg int) {
+	user_metrics = metrics{
+		Owner: owner, Repo: repo, Languages: languages, Commit_Average: commit_avg,
 	}
+}
+
+func init_server() {
+	router := gin.Default()
+	router.Use(cors.Default())
+	router.GET("/metrics", getMetrics)
+	router.Run("localhost:9090")
+}
+
+func main() {
+	var owner = "torvalds"
+	ctx := context.Background()
+	client := github.NewClient(nil)
+	input_repo := "linux"
+	commit_activity, _, err := client.Repositories.ListCommitActivity(ctx, owner, input_repo)
+	if err != nil {
+		println("Error: ", err.Error())
+		return
+	}
+	languages, _, err := client.Repositories.ListLanguages(ctx, owner, input_repo)
+	if err != nil {
+		println("Error: ", err.Error())
+		return
+	}
+	var commit_avg int = 0
+	for _, week := range commit_activity {
+		commit_avg += *week.Total
+	}
+	commit_avg /= 52
+	populate_metrics(owner, input_repo, languages, commit_avg)
+	init_server()
 }
