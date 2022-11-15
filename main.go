7d3d530a473f6c081a0612a624305bd677e8881d
ni-sauvage <sauvagen@tcd.ie>
2022-11-15 19:41:42 +0000

    Refactored, added blocking

--- main.go
+++ main.go
@@ -28,6 +28,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"time"
 
 	"github.com/google/go-github/v48/github"
 )
@@ -45,28 +46,57 @@ func getApiData(url string) ([]byte, error) {
 	}
 	return responseData, nil
 }
+
+func isBlocking(err error) bool {
+	if err == nil || err.Error() != "job scheduled on GitHub side; try again later" {
+		return false
+	}
+	return true
+}
+
 func main() {
 	var owner = ""
 	print("Please enter a github username you would like to query, or type \"quit\" to quit: ")
 	fmt.Scan(&owner)
+
 	for owner != "quit" {
+
 		ctx := context.Background()
 		client := github.NewClient(nil)
+
 		input_repo := ""
 		print("Please enter a repo associated with that github username: ")
 		fmt.Scan(&input_repo)
-		commit_activity, _, err := client.Repositories.ListCommitActivity(ctx, owner, input_repo)
-		if err != nil {
-			println("Error: ", err.Error())
+
+		var commit_activity []*github.WeeklyCommitActivity
+		var err error = &github.AcceptedError{}
+		var languages map[string]int
+		blocking := true
+
+		for blocking {
+
+			commit_activity, _, err = client.Repositories.ListCommitActivity(ctx, owner, input_repo)
+			if err != nil && !isBlocking(err) {
+				println("Error: ", err.Error())
+			}
+			blocking = isBlocking(err)
+
+			languages, _, err = client.Repositories.ListLanguages(ctx, owner, input_repo)
+			if err != nil && !isBlocking(err) {
+				println("Error: ", err.Error())
+			}
+
+			blocking = isBlocking(err) || blocking
+			if blocking {
+				time.Sleep(1 * time.Second)
+			}
 		}
-		languages, _, err := client.Repositories.ListLanguages(ctx, owner, input_repo)
-		if err != nil {
-			println("Error: ", err.Error())
-		}
+
 		var commit_avg int = 0
 		for _, week := range commit_activity {
 			commit_avg += *week.Total
 		}
+
 		commit_avg /= len(commit_activity)
 		println("Languages: ", fmt.Sprint(languages), "\n",
 			"Average weekly commits over past year: ", commit_avg)
