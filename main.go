23ccb53600f34d7d9c8556301512845e3a21d5a8
Niall Sauvage <sauvagen@tcd.ie>
2022-11-15 20:36:36 +0000

    Changed average commits to be a float

--- main.go
+++ main.go
@@ -40,7 +40,7 @@ type metrics struct {
 	Owner          string         `json:"owner"`
 	Repo           string         `json:"repo"`
 	Languages      map[string]int `json:"languages"`
-	Commit_Average int            `json:"average_commits_this_year"`
+	Commit_Average float64        `json:"average_commits_this_year"`
 }
 
 var user_metrics = metrics{}
@@ -50,7 +50,7 @@ func getMetrics(context *gin.Context) {
 	context.IndentedJSON(http.StatusOK, user_metrics)
 }
 
-func populate_metrics(owner, repo string, languages map[string]int, commit_avg int) {
+func populate_metrics(owner, repo string, languages map[string]int, commit_avg float64) {
 	user_metrics = metrics{
 		Owner: owner, Repo: repo, Languages: languages, Commit_Average: commit_avg,
 	}
@@ -85,12 +85,11 @@ func isBlocking(err error) bool {
 }
 
 func main() {
-	var owner = "torvalds"
-  input_repo := "linux"
+	owner := "torvalds"
+	input_repo := "linux"
 
 	ctx := context.Background()
 	client := github.NewClient(nil)
-
 
 	var commit_activity []*github.WeeklyCommitActivity
 	var err error = &github.AcceptedError{}
@@ -109,19 +108,19 @@ func main() {
 		if err != nil && !isBlocking(err) {
 			println("Error: ", err.Error())
 		}
-    
+
 		blocking = isBlocking(err) || blocking
 		if blocking {
 			time.Sleep(1 * time.Second)
 		}
 	}
 
-	var commit_avg int = 0
+	var commit_avg float64 = 0.0
 	for _, week := range commit_activity {
-		commit_avg += *week.Total
+		commit_avg += float64(*week.Total)
 	}
 
-	commit_avg /= len(commit_activity)
+	commit_avg /= float64(len(commit_activity))
 	println("Languages: ", fmt.Sprint(languages), "\n",
 		"Average weekly commits over past year: ", commit_avg)
 	populate_metrics(owner, input_repo, languages, commit_avg)
