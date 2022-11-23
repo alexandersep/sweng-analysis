9791f7c3ab6b5584b0e4ec0413aa07a455da75e3
WesLemon <wesleyshaw25@gmail.com>
2022-11-23 15:12:00 +0000

    trying to get chart data to display

--- src/components/pieChart.vue
+++ src/components/pieChart.vue
@@ -4,110 +4,80 @@ import { onBeforeMount } from 'vue';
 import AppVue from '../App.vue';
 </script>
 <template>
-    <div class="hello">
-      <canvas id="myChart" width="400" height="100"></canvas>
-    </div>
-  
-  </template>
-  <script>  
-  import axios from 'axios'
-   /*async function getData(){
+  <div class="hello">
+    {{labels[0]}}
+    <canvas id="myChart" width="400" height="100"></canvas>
+  </div>
+
+</template>
+<script>
+import axios from 'axios'
+/*async function getData(){
 const data={
-  figures:[
-    'AppVue.data.methods'
-  ],
-  lines:[
-  'AppVue.data.methods'
-  ]
+figures:[
+ 'AppVue.data.methods'
+],
+lines:[
+'AppVue.data.methods'
+]
 }
 const resultSet = await AppVue.load(getData);
-  return resultSet.tablePivot().map(row=>({
-    label: parse(row['AppVue.data.methods']),
-    data: parseInt(row['AppVue.data.methods'])
-  }))
-  };*/
-  export default{
-    data: () => ({
-    // loaded: false,
+return resultSet.tablePivot().map(row=>({
+ label: parse(row['AppVue.data.methods']),
+ data: parseInt(row['AppVue.data.methods'])
+}))
+};*/
+export default {
+  data: () => ({
     chartData: null,
+    labels: [],
+    datasets: []
   }),
   beforeMount() {
-    this.loaded = false
     var path = "http://localhost:9090/metrics";
     axios.get(path)
       .then((res) => {
         const languages = res.data.languages;
-        const labels = [];
-        const datasets = [];
         Object.keys(languages).forEach(key => {
-          labels.push(key);
-          datasets.push(languages[key])
+          this.labels.push(key);
+          this.datasets.push(languages[key])
         })
       })
-    },
-    mounted(){
-        this.loaded=true
-  console.log("component mounted")
-  const ctx = document.getElementById('myChart')
-  data: () => ({
-    // loaded: false,
-    chartData: null,
-  }),
-    this.loaded = false
-    var path = "http://localhost:9090/metrics";
-    axios.get(path)
-      .then((res) => {
-        const languages = res.data.languages;
-        const labels = [];
-        const datasets = [];
-        Object.keys(languages).forEach(key => {
-          labels.push(key);
-          datasets.push(languages[key])
-        })
-      })
-  //this.languages=AppVue.data
-  const data={
-  //const: language=AppVue.data,
+  },
+  mounted() {
+    this.loaded = true
+    console.log("component mounted")
+    const ctx = document.getElementById('myChart')
+    const data = {
+      labels: [this.labels],
 
- /* method:{
-        get_languages : function(){
-        var path = 'http://localhost:9090/metrics'
-      axios.get(path)
-      .then((res) => {
-        this.languages = res.data.languages;
-       // console.log(this.languages);
-      })
-        },
-        beforeMount(){
-       this.get_languages
-        }
-    },*/
-      labels:[this.labels],
-        //label.pull()
-         //here- where we should import the languages section of the data(from get metrics method?)
-//AppVue.methods.get_metrics()
-      /*'Assembly',
-          'C',
-          'C++',
-          'Matlab',
-          'Python',
-          'Shell',
-          'XS'*/
-     
-      datasets:[{
-          label:' percentage of language used',
-          data:[
+      datasets: [{
+        label: 'percentage',
+        data: [
           // getData.data,
-            9562027,
-          1172686451/100,
+          9562027,
+          1172686451 / 100,
           306510,
           2482,
           1343777,
           3836500,
           1239
-      ],
-      backgroundColor: [
-        //enough for additions to be made
+        ],
+
+        backgroundColor: [
+          //enough for additions to be made
+          'rgb(255, 99, 132)',
+          'rgb(54, 162, 235)',
+          'rgb(255, 205, 86)',
+          'rgb(0, 120, 0)',
+          'rgb(219, 255, 51)',
+          'rgb(51, 255, 189)',
+          'rgb(131, 51, 255)',
+          'rgb(252, 51, 255)',
+          'rgb(16, 2, 16)',
+          'rgb(233, 113, 235)',
+          'rgb(235, 172, 113)',
+          'rgb(238, 236, 228)',
           'rgb(255, 99, 132)',
           'rgb(54, 162, 235)',
           'rgb(255, 205, 86)',
@@ -122,32 +92,35 @@ const resultSet = await AppVue.load(getData);
           'rgb(238, 236, 228)'
         ]
       }],
-     hoverOffset:4,
+      hoverOffset: 4,
+    }
+    const myChart = new Chart(ctx, {
+      type: 'doughnut',
+      data: data,
+    }
+    );
+    myChart;
   }
-  const myChart=new Chart(ctx,{
-    type: 'doughnut',
-    data: data,
-      }
-  );
-  myChart;
-     }
-   }
-  </script>
+}
+</script>
   
   <!-- Add "scoped" attribute to limit CSS to this component only -->
-  <style scoped>
-  h3 {
-    margin: 40px 0 0;
-  }
-  ul {
-    list-style-type: none;
-    padding: 0;
-  }
-  li {
-    display: inline-block;
-    margin: 0 10px;
-  }
-  a {
-    color: #42b983;
-  }
-  </style>
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
