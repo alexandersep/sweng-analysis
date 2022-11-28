7fe2e64276b4330ee093d96edc57ed92913eafe6
RADRI <radridri@gmail.com>
2022-11-28 20:00:09 +0000

    Make header stop moving on page switch

--- src/App.vue
+++ src/App.vue
@@ -53,7 +53,7 @@ export default {
     <button class="button"  v-if="onGraphs == true" @click="onGraphs = !onGraphs"> Go To Home</button>
 
   </header>
-  
+
   <main v-if="onGraphs == false" class="main">
     <TheWelcome/>
   </main>
@@ -66,52 +66,22 @@ export default {
 
 header {
   line-height: 1.5;
-}
-
-.box {
-  width: 50px;
-  height: 200px;
-  position: absolute;
-  bottom: 20px;
-}
-.box2 {
-  width: 50px;
-  height: 175px;
-  position: absolute;
-  bottom: 20px;
-  left: 50px;
-}
-.box3 {
-  width: 50px;
-  height: 100px;
-  position: absolute;
-  bottom: 20px;
-  left: 100px;
-}
-
-
-.logo {
   display: block;
-  margin: 0 auto 4rem;
-  top: -50px;
+  place-items: center;
+  padding-right: calc(var(--section-gap) / 2);
 }
-
-@media (min-width: 1023px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+.logo {
+    margin: 0 0 50px 0;
   }
-
-  .logo {
-    margin: 0 2rem 0 0;
-    position: absolute;
-  }
-
   header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+    margin: 0 0 -75px 0;
+  }
+  header .button {
+    margin: 0 0 0 0;
+  }
+@media (max-width: 1023px) {
+  .main{
+    top: 100px;
   }
 }
 </style>
