b03b0ade1468302bb7b6e6513583f066ca6967ac
RADRI <radridri@gmail.com>
2022-11-28 20:15:00 +0000

    Make only one graph visible at a time

--- src/components/GraphPage.vue
+++ src/components/GraphPage.vue
@@ -9,34 +9,33 @@
     export default {
     data() {
         return {
-        showBar: false,
-        showLine: false,
-        showPie: false,
+        whichGraph: 0,
         }
     }
     }
 </script>
 
 <template>
-    <button class="button"  @click="showLine = !showLine"> Weekly Commits</button>
-    <button class="button"  @click="showPie = !showPie"> Languages Used</button>
-    <button class="button"  @click="showBar = !showBar"> Top Users</button>
+    <button class="button"  @click="whichGraph = 0"> Weekly Commits</button>
+    <button class="button"  @click="whichGraph = 1"> Languages Used</button>
+    <button class="button"  @click="whichGraph = 2"> Top Users</button>
 
-    <GraphItem v-if="showPie">
+    <GraphItem v-if="whichGraph == 0">
         <template #heading>Languages Used</template>
         <template #description>A pie chart describing the percentage of languages used in a repo.</template>
         <template #graph><pieChart/></template>
     </GraphItem>
 
-    <GraphItem v-if="showBar">
+    <GraphItem v-if="whichGraph == 1">
         <template #heading>Top Users</template>
         <template #description>A bar chart showing the users with the most amount of commits in a repo. </template>
         <template #graph><barChart/></template>
     </GraphItem>
 
-    <GraphItem v-if="showLine">
+    <GraphItem v-if="whichGraph == 2">
         <template #heading>Weekly Commits</template>
         <template #description>A line chart describing the timeframe of weekly commits. </template>
         <template #graph><lineChart/></template>
     </GraphItem>
 </template>
+
